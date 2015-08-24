080febf5edfa2173d34d9fdba2f386cf4d6b3d09
pavlo_vavruk <pavlo.vavruk@gmail.com>
2015-08-24 12:43:08 +0200

    Fix after aspect and around aspect function call result

--- lib/water_drop/aspects/after_aspect.rb
+++ lib/water_drop/aspects/after_aspect.rb
@@ -10,7 +10,6 @@ module WaterDrop
     #   )
     class AfterAspect < BaseAspect
       after options[:method], interception_arg: true do |interception, result, *args|
-        # puts result
         options = interception.options
         interception.aspect.handle(self, options, args, options[:message], result)
         result
