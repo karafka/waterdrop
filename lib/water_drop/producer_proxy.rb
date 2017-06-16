0d6c5dbb2c7afcfad533886b0a300e1e06b98ae0
Ghislain 'Aus' Lacroix <aus@castle.io>
2017-06-15 18:26:04 -0700

    Remove commented out bits.

--- lib/water_drop/producer_proxy.rb
+++ lib/water_drop/producer_proxy.rb
@@ -67,14 +67,9 @@ module WaterDrop
 
       if ::WaterDrop.config.kafka.producer.use_async_producer
         @producer ||= @kafka.async_producer(
-          # sync_producer: @kafka.producer(
-          #   max_buffer_size: ::WaterDrop.config.kafka.producer.max_buffer_size,
-          #   max_buffer_bytesize: ::WaterDrop.config.kafka.producer.max_buffer_bytesize
-          # ),
           max_queue_size: ::WaterDrop.config.kafka.producer.max_queue_size,
           delivery_threshold: ::WaterDrop.config.kafka.producer.delivery_threshold,
           delivery_interval: ::WaterDrop.config.kafka.producer.delivery_interval,
-          #logger: ::WaterDrop.logger
         )
       else
         @producer ||= @kafka.producer(
