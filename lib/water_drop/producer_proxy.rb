8c3322cacc0dc44b4214445e023b6f5a0282bd5e
Ghislain 'Aus' Lacroix <aus@castle.io>
2017-06-15 17:45:03 -0700

    Bug: get rid of private method.

--- lib/water_drop/producer_proxy.rb
+++ lib/water_drop/producer_proxy.rb
@@ -67,14 +67,20 @@ module WaterDrop
 
       if ::WaterDrop.config.kafka.producer.use_async_producer
         @producer ||= @kafka.async_producer(
-          sync_producer: self.create_sync_producer,
+          sync_producer: @kafka.producer(
+            max_buffer_size: ::WaterDrop.config.kafka.producer.max_buffer_size,
+            max_buffer_bytesize: ::WaterDrop.config.kafka.producer.max_buffer_bytesize
+          ),
           max_queue_size: ::WaterDrop.config.kafka.producer.max_queue_size,
           delivery_threshold: ::WaterDrop.config.kafka.producer.delivery_threshold,
           delivery_interval: ::WaterDrop.config.kafka.producer.delivery_threshold,
           logger: ::WaterDrop.logger
         )
       else
-        @producer ||= self.create_sync_producer
+        @producer ||= @kafka.producer(
+          max_buffer_size: ::WaterDrop.config.kafka.producer.max_buffer_size,
+          max_buffer_bytesize: ::WaterDrop.config.kafka.producer.max_buffer_bytesize
+        )
       end
     end
 
@@ -89,15 +95,5 @@ module WaterDrop
       @producer&.shutdown
       @producer = nil
     end
-
-  private
-    def create_sync_producer
-      sync_producer = @kafka.producer(
-        max_buffer_size: ::WaterDrop.config.kafka.producer.max_buffer_size,
-        max_buffer_bytesize: ::WaterDrop.config.kafka.producer.max_buffer_bytesize
-      )
-      sync_producer
-    end
-
   end
 end
