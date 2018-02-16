7e1e7a0654c7fd672c735d0f8c5605a5a91ce20e
Maciej Mensfeld <maciej@mensfeld.pl>
2018-02-16 10:11:40 +0100

    listener as a module

--- lib/water_drop/instrumentation/listener.rb
+++ lib/water_drop/instrumentation/listener.rb
@@ -4,42 +4,45 @@ module WaterDrop
   module Instrumentation
     # Default listener that hooks up to our instrumentation and uses its events for logging
     # It can be removed/replaced or anything without any harm to the Waterdrop flow
-    class Listener
-      class << self
-        %i[
-          sync_producer
-          async_producer
-        ].each do |producer_type|
-          define_method :"on_#{producer_type}_call_error" do |event|
-            options = event[:options]
-            error = event[:error]
-            attempts_count = event[:attempts_count]
+    # @note It is a module as we can use it then as a part of the Karafka framework listener
+    #   as well as we can use it standalone
+    module Listener
+      # Log levels that we use in this particular listener
+      USED_LOG_LEVELS = %i[
+        info
+        error
+      ].freeze
 
-            error "Delivery failure to: #{options} because of #{error}"
-          end
+      %i[
+        sync_producer
+        async_producer
+      ].each do |producer_type|
+        define_method :"on_#{producer_type}_call_error" do |event|
+          options = event[:options]
+          error = event[:error]
+          attempts_count = event[:attempts_count]
 
-          define_method :"on_#{producer_type}_call_retry" do |event|
-            attempts_count = event[:attempts_count]
-            options = event[:options]
-            error = event[:error]
-
-            info "Attempt #{attempts_count} of delivery to: #{options} because of #{error}"
-          end
+          error "Delivery failure to: #{options} because of #{error}"
         end
 
-        private
+        define_method :"on_#{producer_type}_call_retry" do |event|
+          attempts_count = event[:attempts_count]
+          options = event[:options]
+          error = event[:error]
 
-        %i[
-          debug
-          info
-          error
-          fatal
-        ].each do |log_method|
-          define_method log_method do |*args|
-            WaterDrop.logger.send(log_method, *args)
-          end
+          info "Attempt #{attempts_count} of delivery to: #{options} because of #{error}"
         end
       end
+
+      USED_LOG_LEVELS.each do |log_level|
+        define_method log_level do |*args|
+          WaterDrop.logger.send(log_level, *args)
+        end
+      end
+
+      extend self
+
+      USED_LOG_LEVELS.each(&method(:private_class_method))
     end
   end
 end
