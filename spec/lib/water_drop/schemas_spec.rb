12c62921f68a82d9119de776c1a3fdeab0756a59
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-25 17:22:01 +0200

    spec context desc fix

--- spec/lib/water_drop/schemas_spec.rb
+++ spec/lib/water_drop/schemas_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe WaterDrop::Schemas do
   describe '#TOPIC_REGEXP' do
     subject(:match) { input.match? described_class::TOPIC_REGEXP }
 
-    context 'valid topic' do
+    context 'when topic name is valid' do
       let(:input) { 'name' }
 
       it { is_expected.to eq true }
     end
 
-    context 'invalid topic' do
+    context 'when topic name is invalid' do
       let(:input) { '$%^&*' }
 
       it { is_expected.to eq false }
