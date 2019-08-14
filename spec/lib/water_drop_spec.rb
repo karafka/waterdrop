5ba4a4cab767d45c39524c8969fa919f775c814c
Maciej Mensfeld <maciej@mensfeld.pl>
2019-08-14 16:28:16 +0200

    water_drop module methods specs

--- spec/lib/water_drop_spec.rb
+++ spec/lib/water_drop_spec.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe WaterDrop do
-  pending
+  subject(:water_drop) { described_class }
+
+  describe '#gem_root' do
+    context 'when we want to get gem root path' do
+      let(:path) { Dir.pwd }
+
+      it { expect(water_drop.gem_root.to_path).to eq path }
+    end
+  end
 end
