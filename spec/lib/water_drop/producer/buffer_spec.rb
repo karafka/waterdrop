32b4587304ae8bd13b2ee3866aaaa119679f20c0
Maciej Mensfeld <maciej@mensfeld.pl>
2019-09-12 15:08:59 +0200

    buffer specs

--- spec/lib/water_drop/producer/buffer_spec.rb
+++ spec/lib/water_drop/producer/buffer_spec.rb
@@ -4,11 +4,49 @@ RSpec.describe WaterDrop::Producer::Buffer do
   subject(:producer) { build(:producer) }
 
   describe '#buffer' do
-    pending
+    subject(:buffering) { producer.buffer(message) }
+
+    context 'when message is invalid' do
+      let(:message) { build(:invalid_message) }
+
+      it { expect { buffering }.to raise_error(WaterDrop::Errors::MessageInvalidError) }
+    end
+
+    context 'when message is valid' do
+      let(:message) { build(:valid_message) }
+
+      it { expect(buffering).to include(message) }
+    end
   end
 
   describe '#buffer_many' do
-    pending
+    subject(:buffering) { producer.buffer_many(messages) }
+
+    context 'when we have several invalid messages' do
+      let(:messages) { Array.new(10) { build(:invalid_message) } }
+
+      it { expect { buffering }.to raise_error(WaterDrop::Errors::MessageInvalidError) }
+    end
+
+    context 'when the last message out of a batch is invalid' do
+      let(:messages) { [build(:valid_message), build(:invalid_message)] }
+
+      before { allow(producer.client).to receive(:produce) }
+
+      it { expect { buffering }.to raise_error(WaterDrop::Errors::MessageInvalidError) }
+
+      it 'expect to never reach the client so no messages arent sent' do
+        expect(producer.client).not_to have_received(:produce)
+      end
+    end
+
+    context 'when we have several valid messages' do
+      let(:messages) { Array.new(10) { build(:valid_message) } }
+
+      it 'expect all the results to be buffered' do
+        expect(buffering).to eq(messages)
+      end
+    end
   end
 
   describe '#flush_async' do
