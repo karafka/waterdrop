78e5e268d969195b6aa368658fb813d549a2cc1d
Maciej Mensfeld <maciej@mensfeld.pl>
2019-10-08 15:05:49 +0200

    missing specs

--- spec/lib/water_drop/producer_spec.rb
+++ spec/lib/water_drop/producer_spec.rb
@@ -1,16 +1,68 @@
 # frozen_string_literal: true
 
 RSpec.describe WaterDrop::Producer do
+  subject(:producer) { described_class.new }
+
   describe '#initialize' do
-    pending
+    context 'when we initialize without a setup' do
+      it { expect { producer }.not_to raise_error }
+      it { expect(producer.status.active?).to eq(false) }
+    end
+
+    context 'when initializing with setup' do
+      subject(:producer) do
+        described_class.new do |config|
+          config.deliver = false
+          config.kafka = { 'bootstrap.servers' => 'localhost:9092' }
+        end
+      end
+
+      it { expect { producer }.not_to raise_error }
+      it { expect(producer.status.active?).to eq(true) }
+    end
   end
 
   describe '#setup' do
-    pending
+    context 'when producer has already been configured' do
+      subject(:producer) { build(:producer) }
+
+      let(:expected_error) { WaterDrop::Errors::ProducerAlreadyConfiguredError }
+
+      it { expect { producer.setup {} }.to raise_error(expected_error) }
+    end
+
+    context 'when producer was not yet configured' do
+      let(:setup) do
+        lambda { |config|
+          config.deliver = false
+          config.kafka = { 'bootstrap.servers' => 'localhost:9092' }
+        }
+      end
+
+      it { expect { producer.setup(&setup) }.not_to raise_error }
+    end
   end
 
   describe '#close' do
-    pending
+    subject(:producer) { build(:producer) }
+
+    context 'when producer already closed' do
+      before { producer.close }
+
+      it { expect { producer.close }.not_to raise_error }
+      it { expect(producer.tap(&:close).status.closed?).to eq(true) }
+    end
+
+    context 'when producer was not yet closed' do
+      it { expect { producer.close }.not_to raise_error }
+      it { expect(producer.tap(&:close).status.closed?).to eq(true) }
+    end
+
+    context 'when there were messages in the buffer' do
+      before { producer.buffer(build(:valid_message)) }
+
+      it { expect { producer.close }.to change { producer.messages.size }.from(1).to(0) }
+    end
   end
 
   describe '#ensure_active!' do
