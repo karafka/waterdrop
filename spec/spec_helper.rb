f35b70f1b0df10aa9bff0776a6722368e976f479
Maciej Mensfeld <maciej@mensfeld.pl>
2020-04-23 15:41:26 +0200

    test sleep on segfault

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.after(:all) { sleep 1 }
 end
 
 require 'water_drop'
