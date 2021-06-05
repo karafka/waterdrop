5d27b6680850107e96dae3f454411f9404d9a628
Maciej Mensfeld <maciej@mensfeld.pl>
2021-06-05 12:06:00 +0200

    undefine finalizer based on id upon closing

--- lib/water_drop/producer.rb
+++ lib/water_drop/producer.rb
@@ -99,7 +99,7 @@ module WaterDrop
 
           # No need for auto-gc if everything got closed by us
           # This should be used only in case a producer was not closed properly and forgotten
-          ObjectSpace.undefine_finalizer(self)
+          ObjectSpace.undefine_finalizer(id)
 
           # Flush has it's own buffer mutex but even if it is blocked, flushing can still happen
           # as we close the client after the flushing (even if blocked by the mutex)
