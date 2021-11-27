346efd196928e802d1d4e4377bb7d9fdcfb16761
Maciej Mensfeld <maciej@mensfeld.pl>
2021-11-27 17:31:43 +0100

    more specs

--- spec/lib/water_drop/instrumentation_spec.rb
+++ spec/lib/water_drop/instrumentation_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
-  pending
+  subject(:instrumentation) { described_class }
+
+  describe '#statistics_callbacks' do
+    it { expect(instrumentation.statistics_callbacks).to be_a(described_class::CallbacksManager) }
+  end
+
+  describe '#error_callbacks' do
+    it { expect(instrumentation.error_callbacks).to be_a(described_class::CallbacksManager) }
+  end
+
+  it 'expect to have separate manager for each type of callbacks' do
+    expect(instrumentation.statistics_callbacks).not_to eq(instrumentation.error_callbacks)
+  end
 end
