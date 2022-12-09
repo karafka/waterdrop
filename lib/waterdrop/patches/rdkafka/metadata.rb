e7457dde8fa21eaf209bcf981f88fed864957485
Maciej Mensfeld <maciej@mensfeld.pl>
2022-12-09 15:14:42 +0100

    retry metadata requests upon timeout

---
+++ lib/waterdrop/patches/rdkafka/metadata.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module WaterDrop
+  # Patches to external components
+  module Patches
+    # Rdkafka related patches
+    module Rdkafka
+      # Rdkafka::Metadata patches
+      module Metadata
+        # We overwrite this method because there were reports of metadata operation timing out
+        # when Kafka was under stress. While the messages dispatch will be retried, this was not
+        # and was causing problems.
+        #
+        # @param args [Array<Object>] all the metadata original arguments
+        def initialize(*args)
+          attempt ||= 0
+          attempt += 1
+
+          super(*args)
+        rescue Rdkafka::RdkafkaError => e
+          backoff_factor = 2 ** attempt
+          timeout = backoff_factor * 0.1
+
+          sleep(attempt)
+
+          raise if attempt > 10
+          retry if e.code == :timed_out
+          raise(e)
+        end
+      end
+    end
+  end
+end
+
+::Rdkafka::Metadata.prepend ::WaterDrop::Patches::Rdkafka::Metadata
