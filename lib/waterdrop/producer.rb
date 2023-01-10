776dfd4cf0f1c7b86d90630b432d164d957deeba
Maciej Mensfeld <maciej@mensfeld.pl>
2023-01-10 13:38:42 +0100

    test

--- lib/waterdrop/producer.rb
+++ lib/waterdrop/producer.rb
@@ -1,5 +1,52 @@
 # frozen_string_literal: true
 
+# frozen_string_literal: true
+
+module Rdkafka
+  # @private
+  # A wrapper around a native kafka that handles all access
+  class NativeKafka
+    def initialize(inner)
+      @inner = inner
+      @mutex = Mutex.new
+
+      # Start thread to poll client for delivery callbacks
+      @polling_thread = Thread.new do
+        loop do
+          Rdkafka::Bindings.rd_kafka_poll(@inner, 250)
+          # Destroy consumer and exit thread if closing and the poll queue is empty
+          break if Thread.current[:closing] && Rdkafka::Bindings.rd_kafka_outq_len(@inner) == 0
+        end
+      end
+
+      @polling_thread.abort_on_exception = true
+      @polling_thread[:closing] = false
+
+      @closing = false
+    end
+
+    def close(object_id=nil)
+      return if closed?
+
+      # Indicate to the outside world that we are closing
+      @closing = true
+
+      # Indicate to polling thread that we're closing
+      @polling_thread[:closing] = true
+
+      @mutex.synchronize do
+        return unless @inner
+
+        # Wait for the polling thread to finish up
+        @polling_thread.join
+
+        Rdkafka::Bindings.rd_kafka_destroy(@inner)
+        @inner = nil
+      end
+    end
+  end
+end
+
 module WaterDrop
   # Main WaterDrop messages producer
   class Producer
