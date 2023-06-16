be954f691da4f15c48db62ddc6ca6888744c4f13
Maciej Mensfeld <maciej@mensfeld.pl>
2023-06-16 12:48:23 +0200

    fix

--- spec/lib/waterdrop/producer_spec.rb
+++ spec/lib/waterdrop/producer_spec.rb
@@ -307,8 +307,8 @@ RSpec.describe_current do
     context 'when producer not in use' do
       it 'expect to work correctly' do
         pid = fork { producer.produce_sync(topic: 'test', payload: '1') }
-        Process.wait(pid) unless $?.exited?
-        expect($?.to_i).to eq(0)
+        Process.wait(pid) unless $CHILD_STATUS.exited?
+        expect($CHILD_STATUS.to_i).to eq(0)
       end
     end
   end
