f1cc987fd8e547828e05d2be56b1750e2d2acf1b
Maciej Mensfeld <maciej@mensfeld.pl>
2026-04-18 10:25:49 +0200

    stabilize specs

--- test/lib/waterdrop/producer/sync_test.rb
+++ test/lib/waterdrop/producer/sync_test.rb
@@ -84,6 +84,7 @@ describe_current do
     context "when allow.auto.create.topics is set to false" do
       before do
         @message = build(:valid_message)
+        create_topic(@message[:topic])
         @producer = build(
           :producer,
           kafka: {
@@ -103,6 +104,7 @@ describe_current do
     context "when allow.auto.create.topics is set to false and we use partition key" do
       before do
         @message = build(:valid_message, partition_key: "test", key: "test")
+        create_topic(@message[:topic])
         @producer = build(
           :producer,
           kafka: {
